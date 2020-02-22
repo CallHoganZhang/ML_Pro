92bb46a02b3801f448f4c9c3dd7b4d2dd75a46c0
CallHoganZhang <675587543@qq.com>
2020-02-22 21:17:35 +0800

    change feature choice files

--- tree_weather/feature_choice.py
+++ tree_weather/feature_choice.py
@@ -4,36 +4,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
+import data_load
 
-features = pd.read_csv('data/temps_extended.csv')
-features = pd.get_dummies(features)
+def encoding_features(features):
+    onehot_coding_features = pd.get_dummies(features) #one hot coding
+    onehot_coding_labels = onehot_coding_features['actual']
+    dropped_features = onehot_coding_features.drop('actual', axis = 1)
+    dropped_features_list = list(dropped_features.columns)
 
-labels = features['actual']
-features = features.drop('actual', axis = 1)
+    array_features = np.array(dropped_features)
+    array_labels = np.array(onehot_coding_labels)
+    return array_features, array_labels, dropped_features_list
 
-feature_list = list(features.columns)
-
-features = np.array(features)
-labels = np.array(labels)
-
-train_features, test_features, train_labels, test_labels = train_test_split(features, labels,
-                                                                            test_size = 0.1, random_state = 42)
-
-print('Training Features Shape:', train_features.shape)
-print('Training Labels Shape:', train_labels.shape)
-print('Testing Features Shape:', test_features.shape)
-print('Testing Labels Shape:', test_labels.shape)
-
-print('{:0.1f} years of data in the training set'.format(train_features.shape[0] / 365.))
-print('{:0.1f} years of data in the test set'.format(test_features.shape[0] / 365.))
-
-important_feature_names = ['temp_1', 'average', 'ws_1', 'temp_2', 'friend', 'year']
-important_indices = [feature_list.index(feature) for feature in important_feature_names]
-important_train_features = train_features[:, important_indices]
-important_test_features = test_features[:, important_indices]
-
-print('Important train features shape:', important_train_features.shape)
-print('Important test features shape:', important_test_features.shape)
+def get_important_list(feature_list):
+    important_feature_names = ['temp_1', 'average', 'ws_1', 'temp_2', 'friend', 'year']
+    important_indices = [feature_list.index(feature) for feature in important_feature_names]
+    return important_indices
 
 def evaluate(model, test_features, test_labels):
     predictions = model.predict(test_features)
@@ -43,6 +29,26 @@ def evaluate(model, test_features, test_labels):
 
     print('平均气温误差.',np.mean(errors))
     print('Accuracy = {:0.2f}%.'.format(accuracy))
+
+array_features, array_labels, dropped_features_list = encoding_features(data_load.features)
+train_features, test_features, train_labels, test_labels = train_test_split(array_features, array_labels, test_size = 0.1, random_state = 0)
+
+'''
+print('Training Features Shape:', train_features.shape)
+print('Training Labels Shape:', train_labels.shape)
+print('Testing Features Shape:', test_features.shape)
+print('Testing Labels Shape:', test_labels.shape)
+
+print('{:0.1f} years of data in the training set'.format(train_features.shape[0] / 365.))
+print('{:0.1f} years of data in the test set'.format(test_features.shape[0] / 365.))
+'''
+
+important_indices = get_important_list(dropped_features_list)
+important_train_features = train_features[:, important_indices]
+important_test_features = test_features[:,important_indices ]
+
+print('Important train features shape:', important_train_features.shape)
+print('Important test features shape:', important_test_features.shape)
 
 rf = RandomForestRegressor(random_state = 42)
 
@@ -60,16 +66,20 @@ random_grid = {'n_estimators': n_estimators,
                'min_samples_leaf': min_samples_leaf,
                'bootstrap': bootstrap}
 
-# rf = RandomForestRegressor()
-#
-# rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid,
-#                               n_iter = 10, scoring='neg_mean_absolute_error',
-#                               cv = 3, verbose=2, random_state=42, n_jobs=-1)
 # rf_random.fit(train_features, train_labels)
 # print('best_params_', rf_random.best_params_)
-#
+
 # best_random = rf_random.best_estimator_
 # evaluate(best_random, test_features, test_labels)
+
+
+def rfFit(randomForest, model, param):
+    rf_random = model(estimator=randomForest, param_distributions=param,
+                              n_iter = 10, scoring='neg_mean_absolute_error',
+                              cv = 3, verbose=2, random_state=42, n_jobs=-1)
+    return rf_random
+
+randomForestResult = rfFit(RandomForestRegressor(), RandomizedSearchCV, random_grid)
 
 param_grid = {
     'bootstrap': [True],
